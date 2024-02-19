6cea7e7561215f389f7055685977c89a045ed324
Joan Kenedy <joankenedi9311@gmail.com>
2024-02-19 17:49:05 -0600

    seguimos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { BoxVideoComponent } from './box-video/box-video.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { CartaMenuComponent } from './carta-menu/carta-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, BoxVideoComponent, NosotrosComponent, CartaMenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
