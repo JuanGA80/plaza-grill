bc0e448fe5c5926e8a6bada748b9f38c7a8db63b
Joan Kenedy <joankenedi9311@gmail.com>
2024-02-21 17:34:49 -0600

    galeria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { MenuComponent } from './menu/menu.component';
 import { BoxVideoComponent } from './box-video/box-video.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { CartaMenuComponent } from './carta-menu/carta-menu.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { GaleryComponent } from './galery/galery.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, BoxVideoComponent, NosotrosComponent, CartaMenuComponent],
+  declarations: [AppComponent, MenuComponent, BoxVideoComponent, NosotrosComponent, CartaMenuComponent, DeliveryComponent, GaleryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
---
+++ src/app/galery/galery.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galery',
+  templateUrl: './galery.component.html',
+  styleUrl: './galery.component.css',
+})
+export class GaleryComponent {}
